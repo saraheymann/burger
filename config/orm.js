1e0b6dac517bda7060dc1b0288c33e57890d9c7e
sara heymann <saraheymann@saras-MacBook-Pro.local>
2017-08-15 22:08:04 -0500

    folder set-up

---
+++ config/orm.js
@@ -0,0 +1,5 @@
+// import (require) connection.js
+// create methods that execute the necessary mysql commands in the controllers.
+// these are the methods you will need to use in order to retrieve and store data
+// in the database - selectAll(), insertOne(), updateOne()
+// export the ORM object in module.exports
